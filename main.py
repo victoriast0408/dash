cb3ffa8918f327a2302d44f7c9fbfa9e423bc6ac
Victoria <victoria@Victorias-MacBook-Air.local>
2019-12-02 20:39:38 +0200

    Addea a better design for 4 cards

--- main.py
+++ main.py
@@ -87,53 +87,77 @@ app.layout = html.Div(children=[
 
     # The second row with cards
     dbc.Row([
-                    dbc.Col(
-                        dbc.Card([
-                            dbc.CardHeader(html.H3("Today: ", className="card-title")),
-                            dbc.CardBody(
-                                [
-                                    html.H4('30 January 2020', className="text-center")
-                                    #dbc.CardLink("Card link", href="#"),
-                                    #dbc.CardLink("External link", href="https://google.com"),
-                                ]
-                            ),
-                            #style={"width": "18rem"},
-                        ], className="card h-100")
-                    ),
-                    dbc.Col(dbc.Card([
-                            dbc.CardHeader(
-                                    dcc.Tabs(id="tabs-aver-bill", value='tab-1-aver-bill', children=[
-                                        dcc.Tab(label='Branch 1', value='tab-1-aver-bill'),
-                                        dcc.Tab(label='Branch 2', value='tab-2-aver-bill'),
-                                    ]),
-                            ),
-                            dbc.CardBody(
-                                    # container for new data in tab:
-                                    html.Div(id='tabs-aver-bill-content')
-                            ),
-                            #style={"width": "18rem", "height":"300px"},
-                        ])),
 
-                    dbc.Col(dbc.Card([
-                        dbc.CardHeader(
-                                dcc.Tabs(id="tabs-count-bill", value='tab-1-count-bill', children=[
-                                    dcc.Tab(label='Branch 1', value='tab-1-count-bill'),
-                                    dcc.Tab(label='Branch 2', value='tab-2-count-bill'),
-                                ]),
-                            ),
-                        dbc.CardBody(
-                               # container for new data in tab:
-                                html.Div(id='tabs-count-bill-content'),
-                        ),
-                        #style={"width": "18rem", "height": "300px"},
-                    ])),
-                ], className='pt-3'),
+            html.Div([
+              html.Div(
+                html.Div(
+                  html.Div(
+                    html.Div([
+                      html.Div("Today: ", className="text-xs font-weight-bold text-primary text-uppercase mb-1"),
+                      html.Div("30 January 2020", className="h5 mb-0 font-weight-bold text-gray-800")], className="col mr-2"),
+                    className="col mr-2"), className="card-body"), className="card border-left-primary shadow h-100 py-2"
+                    )], className="col-xl-3 col-md-3 mb-4"),
+
+### Radio buttons
+            html.Div([
+              html.Div(
+                html.Div(
+                  html.Div(
+                    html.Div([
+                    dcc.RadioItems(
+                        id="radio-items",
+                        options =[
+                            {'label':'Branch 1' ,'value': 'first'},
+                            {'label': 'Branch 2', 'value': 'second'}
+                      ], value = "first",
+                        labelStyle={'display': 'inline-block'}),
+                      html.Div(id='aver_content', className="h5 mb-0 font-weight-bold text-gray-800")], className="col mr-2"),
+                        className="col mr-2"), className="card-body"), className="card border-left-primary shadow h-100 py-2"
+                        )], className="col-xl-3 col-md-3 mb-4"),
 
 
-    dbc.Row(html.H3('General Review', className="mx-auto pt-3")),
+### Radio buttons 2
+            html.Div([
+              html.Div(
+                html.Div(
+                  html.Div(
+                    html.Div([
+                    dcc.RadioItems(
+                        id="radio-items2",
+                        options =[
+                            {'label':'Branch 1' ,'value': 'first'},
+                            {'label': 'Branch 2', 'value': 'second'}
+                      ], value = "first",
+                        labelStyle={'display': 'inline-block'}),
+                      html.Div(id='aver_content2', className="h5 mb-0 font-weight-bold text-gray-800")], className="col mr-2"),
+                        className="col mr-2"), className="card-body"), className="card border-left-primary shadow h-100 py-2"
+                        )], className="col-xl-3 col-md-3 mb-4"),
 
-    dbc.Row(dbc.Col(html.Div(
-    dcc.Graph(
+
+### Radio buttons 3
+            html.Div([
+              html.Div(
+                html.Div(
+                  html.Div(
+                    html.Div([
+                    dcc.RadioItems(
+                        id="radio-items3",
+                        options =[
+                            {'label':'Branch 1' ,'value': 'first'},
+                            {'label': 'Branch 2', 'value': 'second'}
+                      ], value = "first",
+                        labelStyle={'display': 'inline-block'}),
+                      html.Div(id='aver_content3', className="h5 mb-0 font-weight-bold text-gray-800")], className="col mr-2"),
+                        className="col mr-2"), className="card-body"), className="card border-left-primary shadow h-100 py-2"
+                        )], className="col-xl-3 col-md-3 mb-4"),
+]),
+
+
+
+dbc.Row(html.H3('General Review', className="mx-auto pt-3")),
+
+dbc.Row(dbc.Col(html.Div(
+dcc.Graph(
         id="line",
         figure= {
             'data': [trace1, trace2],
@@ -155,18 +179,18 @@ app.layout = html.Div(children=[
         },
     )))),
 
-    html.H1(children= ''), # Empty div as a separator
+html.H1(children= ''), # Empty div as a separator
 
-    dbc.Row(html.H3('Additional Information', className="mx-auto pb-3")),
+dbc.Row(html.H3('Additional Information', className="mx-auto pb-3")),
 
-    dbc.Row([
-    dbc.Col(dbc.Card([
+dbc.Row([
+dbc.Col(dbc.Card([
         dbc.CardHeader(
-    dcc.Tabs(id="tabs-example", value='tab-1-example', children=[
+dcc.Tabs(id="tabs-example", value='tab-1-example', children=[
         dcc.Tab(label='Branch 1', value='tab-1-example'),
         dcc.Tab(label='Branch 2', value='tab-2-example'),
     ])),
-        dbc.CardBody([
+dbc.CardBody([
         #container for new pie according to the selected tab:
         html.Div(id='tabs-content-example')])
     ]))
@@ -239,37 +263,53 @@ def render_content(tab):
                 }, )
         ])
 
-# For average bills tabs
-@app.callback(Output('tabs-aver-bill-content', 'children'),
-            [Input('tabs-aver-bill', 'value')])
-def render_content_aver_bill(aver_bill_tab):
-    if aver_bill_tab == 'tab-1-aver-bill':
+# For average bills radio
+@app.callback(Output('aver_content', 'children'),
+            [Input('radio-items', 'value')])
+def update_radio_item_output(value):
+    if value == "first":
         return html.Div([
-                html.H4("Average Bill pro Month: "),
-                html.H4(df['total'].mean().round(2))
+                 html.H4("Average Bill pro Month: "),
+                 html.H4(df['total'].mean().round(2))
             ], className="text-center")
-    elif aver_bill_tab == 'tab-2-aver-bill':
+    elif value == "second":
         return html.Div([
-                html.H4("Average Bill pro Month: "),
+               html.H4("Average Bill pro Month: "),
                 html.H4(df2['total'].mean().round(2))
             ], className="text-center")
 
-# For count bills tabs
-@app.callback(Output('tabs-count-bill-content', 'children'),
-            [Input('tabs-count-bill', 'value')])
-def render_content_count_bill(count_bill_tab):
-    if count_bill_tab == 'tab-1-count-bill':
+# For number of bills radio
+@app.callback(Output('aver_content2', 'children'),
+            [Input('radio-items2', 'value')])
+def update_radio_item_output2(value):
+    if value == "first":
         return html.Div([
-                html.H4("Number of Bills pro Month: "),
-                html.H4(df['bill_number'].count())
-            ], className="text-center")
-    elif count_bill_tab == 'tab-2-count-bill':
+                        html.H4("Number of Bills pro Month: "),
+                        html.H4(df['bill_number'].count())
+                    ], className="text-center")
+    elif value == "second":
         return html.Div([
-                html.H4("Number of Bills pro Month: "),
-                html.H4(df2['bill_number'].count())
-            ], className="text-center")
+                         html.H4("Number of Bills pro Month: "),
+                         html.H4(df2['bill_number'].count())
+                    ], className="text-center")
+
+# For total pro month radio
+@app.callback(Output('aver_content3', 'children'),
+            [Input('radio-items3', 'value')])
+def update_radio_item_output3(value):
+    if value == "first":
+        return html.Div([
+            html.H4("Total amount pro Month: "),
+            html.H4(df['total'].sum())
+        ], className="text-center")
+    elif value == "second":
+        return html.Div([
+            html.H4("Total amount pro Month: "),
+            html.H4(df2['total'].sum())
+        ], className="text-center")
 
     ####
+
 
 if __name__ == '__main__':
     app.run_server()
