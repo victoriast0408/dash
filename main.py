04333bb1de914a494bd86f42fcc38700fb0554db
Victoria <victoriast0408@gmail.com>
2019-12-03 14:25:26 +0200

    added the shadow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datetime import datetime as dt
 import dash
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 import pandas as pd
 import numpy as np
@@ -11,8 +12,10 @@ import plotly.offline as pyo
 # read the file and replace the comma with dot in decimal
 df = pd.read_csv('BillsJan.csv', decimal=',')
 df2 = pd.read_csv('BillsJanFil2.csv', decimal=',')
+df3 = pd.read_csv('warengruppen_fil1.csv')
+df4 = pd.read_csv('warengruppen_fil2.csv')
 
-app = dash.Dash()
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # Group by days and store the unique values in 'day'
 day = df['date'].unique()
@@ -27,13 +30,13 @@ trace1 = go.Scatter(
     x = day,
     y = total_for_each_day,
     mode='lines+markers',
-    name='Filial 1'
+    name='Branch 1'
 )
 trace2 = go.Scatter(
     x = day_fil2,
     y = total_for_each_day_fil2,
     mode='lines+markers',
-    name='Filial 2'
+    name='Branch 2'
 )
 ####
 
@@ -52,14 +55,109 @@ paid_customer_card_sum_fil2 = df2['paid_customer_card'].sum()
 values_fil2 = [paid_cash_sum_fil2, paid_card_sum_fil2, paid_customer_card_sum_fil2]
 ####
 
+# Most popular 15 items
+df_most_pop_item_fil1 = df3.nlargest(15, ['anzahl'])
+most_pop_item_fil1 = df_most_pop_item_fil1['artikel']
+most_pop_item_count_fil1 = df_most_pop_item_fil1['anzahl']
 
-
-
+df_most_pop_item_fil2 = df4.nlargest(15, ['anzahl'])
+most_pop_item_fil2 = df_most_pop_item_fil2['artikel']
+most_pop_item_count_fil2 = df_most_pop_item_fil2['anzahl']
+####
 app.layout = html.Div(children=[
-    html.H1(children= 'Family Bakery'),
-    html.H3(children= 'General Review'),
 
-    dcc.Graph(
+    # Upper navigation bar
+    dbc.NavbarSimple(
+        children=[
+            #dbc.NavItem(dbc.NavLink("Page 1", href="#")),
+            dbc.DropdownMenu(
+                children=[
+                    dbc.DropdownMenuItem("More pages", header=True),
+                ],
+                nav=True,
+                in_navbar=True,
+                label="More",
+            ),
+        ],
+        brand="Family Bakery",
+        brand_href="#",
+        color="#293843",
+        dark=True,
+    ),
+
+    # The second row with cards
+    dbc.Row([
+
+            html.Div([
+              html.Div(
+                html.Div(
+                  html.Div(
+                    html.Div([
+                      html.Div("Today: ", className="text-xs font-weight-bold text-primary text-uppercase mb-1"),
+                      html.Div("30 January 2020", className="h5 mb-0 font-weight-bold text-gray-800")], className="col mr-2"),
+                    className="col mr-2"), className="card-body"), className="card border-left-primary shadow h-100 py-2"
+                    )], className="col-xl-3 col-md-3 mb-4 mt-3"),
+
+### Cards with Radio buttons
+            html.Div([
+              html.Div(
+                html.Div(
+                  html.Div(
+                    html.Div([
+                    dcc.RadioItems(
+                        id="radio-items",
+                        options =[
+                            {'label':'Branch 1' ,'value': 'first'},
+                            {'label': 'Branch 2', 'value': 'second'}
+                      ], value = "first", 
+                        labelStyle={'display': 'inline-block'}, className="mr-4"),
+                      html.Div(id='aver_content', className="h5 mb-0 font-weight-bold text-gray-800")], className="col mr-2"),
+                        className="col mr-2"), className="card-body"), className="card border-left-primary shadow h-100 py-2"
+                        )], className="col-xl-3 col-md-3 mb-4 mt-3"),
+
+
+### Radio buttons 2
+            html.Div([
+              html.Div(
+                html.Div(
+                  html.Div(
+                    html.Div([
+                    dcc.RadioItems(
+                        id="radio-items2",
+                        options =[
+                            {'label':'Branch 1' ,'value': 'first'},
+                            {'label': 'Branch 2', 'value': 'second'}
+                      ], value = "first",
+                        labelStyle={'display': 'inline-block'}),
+                      html.Div(id='aver_content2', className="h5 mb-0 font-weight-bold text-gray-800")], className="col mr-2"),
+                        className="col mr-2"), className="card-body"), className="card border-left-primary shadow h-100 py-2"
+                        )], className="col-xl-3 col-md-3 mb-4 mt-3"),
+
+
+### Radio buttons 3
+            html.Div([
+              html.Div(
+                html.Div(
+                  html.Div(
+                    html.Div([
+                    dcc.RadioItems(
+                        id="radio-items3",
+                        options =[
+                            {'label':'Branch 1' ,'value': 'first'},
+                            {'label': 'Branch 2', 'value': 'second'}
+                      ], value = "first",
+                        labelStyle={'display': 'inline-block'}),
+                      html.Div(id='aver_content3', className="h5 mb-0 font-weight-bold text-gray-800")], className="col mr-2"),
+                        className="col mr-2"), className="card-body"), className="card border-left-primary shadow h-100 py-2"
+                        )], className="col-xl-3 col-md-3 mb-4 mt-3"),
+]),
+
+
+
+#dbc.Row(html.H3('General Review', className="mx-auto pt-3")),
+
+dbc.Row(dbc.Col(html.Div(
+dcc.Graph(
         id="line",
         figure= {
             'data': [trace1, trace2],
@@ -79,20 +177,26 @@ app.layout = html.Div(children=[
             'modeBarButtonsToRemove': ['pan2d', 'lasso2d'],
             'displayModeBar': False        # change to True to display the modebar (plotly tools)
         },
-    ),
+    ), className = "border-left-primary shadow"))),
 
-    html.H1(children= ''), # Empty div as a separator
-    
-    html.H3('Preferable payment method'),    # div with tabs
-    
-    dcc.Tabs(id="tabs-example", value='tab-1-example', children=[
-        dcc.Tab(label='Filial 1', value='tab-1-example'),
-        dcc.Tab(label='Filial 2', value='tab-2-example'),
-    ]),
-    
-    #container for new pie according to the selected tab:
-    html.Div(id='tabs-content-example')        
+html.H1(children= ''), # Empty div as a separator
+
+#dbc.Row(html.H3('Additional Information', className="mx-auto pb-3")),
+
+dbc.Row([
+dbc.Col(dbc.Card([
+        dbc.CardHeader(
+dcc.Tabs(id="tabs-example", value='tab-1-example', children=[
+        dcc.Tab(label='Branch 1', value='tab-1-example'),
+        dcc.Tab(label='Branch 2', value='tab-2-example'),
+    ])),
+dbc.CardBody([
+        #container for new pie according to the selected tab:
+        html.Div(id='tabs-content-example')])
+    ]))
 ])
+
+], className="container-fluid bg-light")
 
 # For tabs
 @app.callback(Output('tabs-content-example', 'children'),
@@ -100,24 +204,112 @@ app.layout = html.Div(children=[
 def render_content(tab):
     if tab == 'tab-1-example':
         return html.Div([
-            #html.H3('Tab content 1'),
             dcc.Graph(
                 id='graph-1-tabs',
                 figure={
-                   'data': [go.Pie(labels=labels, values=values)]        
-                })
+                   'data': [go.Pie(labels=labels, values=values)],
+                    'layout': {
+                    'title': 'Preferable payment method',
+                }},
+                # Remove the "Produced with Plot.ly"
+             config={
+            "displaylogo": False,
+            'modeBarButtonsToRemove': ['pan2d', 'lasso2d'],
+            'displayModeBar': False        # change to True to display the modebar (plotly tools)
+        },),
+            # "15 most popular items" graph
+            dcc.Graph(
+                id='graph-1-1-tabs',
+                figure={
+                    'data': [go.Bar(x= most_pop_item_fil1, y=most_pop_item_count_fil1)],
+                    'layout':{
+                    'title': 'Most popular 15 items',
+                    'yaxis': {'title': 'Number of sold items'}}},
+                # Remove the "Produced with Plot.ly"
+                config={
+                    "displaylogo": False,
+                    'modeBarButtonsToRemove': ['pan2d', 'lasso2d'],
+                    'displayModeBar': False  # change to True to display the modebar (plotly tools)
+                }, )
         ])
     elif tab == 'tab-2-example':
         return html.Div([
-            #html.H3('Tab content 2'),
             dcc.Graph(
                 id='graph-2-tabs',
                 figure={
-                     'data': [go.Pie(labels=labels, values=values_fil2)]
-                    })
+                     'data': [go.Pie(labels=labels, values=values_fil2)],
+                     'layout': {
+                     'title': 'Preferable payment method'}},
+                # Remove the "Produced with Plot.ly"
+                config={
+                    "displaylogo": False,
+                    'modeBarButtonsToRemove': ['pan2d', 'lasso2d'],
+                    'displayModeBar': False  # change to True to display the modebar (plotly tools)
+                },
+            ),
+            # "15 most popular items" graph
+            dcc.Graph(
+                id='graph-2-2-tabs',
+                figure={
+                    'data': [go.Bar(x=most_pop_item_fil2, y=most_pop_item_count_fil2)],
+                    'layout': {
+                        'title': 'Most popular 15 items',
+                        'yaxis': {'title': 'Number of sold items'}}},
+                # Remove the "Produced with Plot.ly"
+                config={
+                    "displaylogo": False,
+                    'modeBarButtonsToRemove': ['pan2d', 'lasso2d'],
+                    'displayModeBar': False  # change to True to display the modebar (plotly tools)
+                }, )
         ])
 
+# For average bills radio
+@app.callback(Output('aver_content', 'children'),
+            [Input('radio-items', 'value')])
+def update_radio_item_output(value):
+    if value == "first":
+        return html.Div([
+                 html.H4("Average Bill pro Month: "),
+                 html.H4(df['total'].mean().round(2))
+            ], className="text-center")
+    elif value == "second":
+        return html.Div([
+               html.H4("Average Bill pro Month: "),
+                html.H4(df2['total'].mean().round(2))
+            ], className="text-center")
+
+# For number of bills radio
+@app.callback(Output('aver_content2', 'children'),
+            [Input('radio-items2', 'value')])
+def update_radio_item_output2(value):
+    if value == "first":
+        return html.Div([
+                        html.H4("Number of Bills pro Month: "),
+                        html.H4(df['bill_number'].count())
+                    ], className="text-center")
+    elif value == "second":
+        return html.Div([
+                         html.H4("Number of Bills pro Month: "),
+                         html.H4(df2['bill_number'].count())
+                    ], className="text-center")
+
+# For total pro month radio
+@app.callback(Output('aver_content3', 'children'),
+            [Input('radio-items3', 'value')])
+def update_radio_item_output3(value):
+    if value == "first":
+        return html.Div([
+            html.H4("Total amount pro Month: "),
+            html.H4(df['total'].sum())
+        ], className="text-center")
+    elif value == "second":
+        return html.Div([
+            html.H4("Total amount pro Month: "),
+            html.H4(df2['total'].sum())
+        ], className="text-center")
+
     ####
+
 
 if __name__ == '__main__':
     app.run_server()
