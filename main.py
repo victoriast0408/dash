d4fca109dce1b31ac98e9f2c3556a6899f989897
Victoria <victoriast0408@gmail.com>
2019-12-09 16:18:51 +0200

    added some .csv files

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ app.layout = html.Div(children=[
                   html.Div(
                     html.Div([
                       html.Div("Today: ", className="text-xs font-weight-bold text-primary text-uppercase mb-1"),
-                      html.Div("30 January 2020", className="h5 mb-0 font-weight-bold text-gray-800"),
+                      html.Div("28 February", className="h5 mb-0 font-weight-bold text-gray-800"),
                     ], className="col mr-2"),
                     className="col mr-2"), className="card-body"), className="card border-left-primary shadow h-100 py-2"
                     )], className="col-xl-3 col-md-3 mb-4 mt-3"),
